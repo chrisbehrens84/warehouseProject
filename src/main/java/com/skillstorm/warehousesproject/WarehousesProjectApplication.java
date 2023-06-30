711b06f6ad60ccfb5a64b436ec6f422ac5add94f
chrisbehrens1984 <cbehrens@skillstorm.com>
2023-06-30 12:34:29 -0700

    initial project files

---
+++ src/main/java/com/skillstorm/warehousesproject/WarehousesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehousesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehousesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillstorm/warehousesproject/WarehousesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehousesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
